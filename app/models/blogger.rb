9fe74b83688a702144e1c1890554963ea51d30b0
afrolambo <stephennoahgalvan@.com>
2020-06-29 15:42:36 -0400

    mostly done blogger

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -1,3 +1,11 @@
 class Blogger < ApplicationRecord
+    has_many :posts 
+    has_many :destinations, through: :posts
 
+    validates :name, presence: true
+    validates :name, uniqueness: {case_sensitive: false}
+
+    validates :age, numericality: {greater_than: 0}
+
+    validates_length_of :bio, minimum: 30
 end
